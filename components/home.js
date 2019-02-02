e8e37b136fd8426026bc74b94dbd47da1e1ac34b
rizkifirdauz <rizkifirdaus92@gmail.com>
2019-02-02 17:03:10 +0700

    tambahmaps

--- components/home.js
+++ components/home.js
@@ -3,6 +3,7 @@ import Header from '../components/header'
 import Section2 from '../components/section2'
 import Section3 from '../components/section3'
 import Section4 from '../components/section4'
+import Maps from '../components/maps'
 
 const Home = () => (
     <div>
@@ -17,6 +18,7 @@ const Home = () => (
         <Section2/>
         <Section3/>
         <Section4/>
+        <Maps/>
     </div>
 )
 export default Home
---
+++ components/maps.js
@@ -0,0 +1,11 @@
+import '../sass/main.scss'
+
+const Maps = () => (
+    <div>
+        <iframe className="maps" 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8246444597844!2d106.72633981431088!3d-6.286766763284322!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f00ba5dbda25%3A0xb7fd685ea56d5988!2sBintaro+Jaya+Xchange!5e0!3m2!1sid!2sid!4v1549101499159" 
+            title="maps" frameborder="0" allowfullscreen>
+        </iframe>
+    </div>
+)
+export default Maps
